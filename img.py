022d1bdf66b8737dc5f89843d564a68f310b7bd3
Deinielle Cruz <deinielle.cruz@gmail.com>
2024-05-01 18:36:39 +0800

    GUI Fixed

--- img.py
+++ img.py
@@ -18,14 +18,17 @@ def dclicked():
     dec.Decrpyt(root)
     
 
+frame = Frame(root)
+frame.pack(padx= 50, pady= 20)
+encrypt = Button(frame, text = "Encrypt" , command=eclicked)
 
-encrypt = Button(root, text = "Encrypt" , command=eclicked)
+decrypt = Button(frame, text = "Decrypt" , command=dclicked)
 
-decrypt = Button(root, text = "Decrypt" , command=dclicked)
-
-
-encrypt.grid(column=1, row=7, ipadx= 5, padx = 5)
-decrypt.grid(column=2, row=7, ipadx= 5, padx = 5)
+title = Label(frame, text= "AES Encryption")
+title.config(font=('arial',20,'bold'))
+title.grid(row= 0 , column= 1 , columnspan= 2, pady=20)
+encrypt.grid(column=1, row=1, ipadx= 5, padx = 5)
+decrypt.grid(column=2, row=1, ipadx= 5, padx = 5)
 
 
 root.mainloop()
